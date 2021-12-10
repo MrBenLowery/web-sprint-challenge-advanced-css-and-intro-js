051f39ed920f1a64e2c53998d49662f26d1e65db
Bennett-Lowery <mrbennettlowery@gmail.com>
2021-12-10 16:07:44 -0600

    finished task 4

--- index.js
+++ index.js
@@ -244,11 +244,17 @@ Example born in 1901 and died in 1959 - included -- born in 1889 and died in 192
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 // Hint - Look up the .split() method
 
-function get20s(/*Your Code Here*/) {
-  /*Your Code Here*/
+function get20s(array) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].years.split(' - ') >= '1900' && array[i].years.split(' - ') <= '2000') {
+      newArray.push(array[i].name)
+    }
+  }
+  return newArray;
 }
 
-
+console.log(get20s(artists));
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 Use removeArtist to do the following:
